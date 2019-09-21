af7111d16de92072c07b61a897b842eb475eec12
Sterling Ford <mellowcf@gmail.com>
2019-09-21 12:58:57 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,11 +43,11 @@ useEffect( ()=>{
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={()=> setHome(home+6)} >Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={()=> setHome(home+7)} >Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={()=> setHome(home+3)} >Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={()=> setAway(away+6)} >Away Touchdown</button>
+          <button className="awayButtons__touchdown" onClick={()=> setAway(away+7)} >Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={()=> setAway(away+3)} >Away Field Goal</button>
         </div>
       </section>
